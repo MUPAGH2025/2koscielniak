0c649dbb4c4c4f44410654effeec16605ae10078
patkosAGH <patkos@student.agh.edu.pl>
2025-12-05 13:45:35 +0100

    Projekt MUP

--- nbconvert_config.py
+++
@@ -1,9 +0,0 @@
-c = get_config()
-
-# use xelatex for better unicode + svg support
-c.LatexExporter.latex_command = ['xelatex', '{filename}']
-
-# enable svg to pdf conversion
-c.SVG2PDFPreprocessor.from_format = 'svg'
-c.SVG2PDFPreprocessor.to_format = 'pdf'
-c.LatexExporter.preprocessors = ['nbconvert.preprocessors.SVG2PDFPreprocessor']
